8222b57e27feb9d3559b359d9d213b92fd0bceb4
Aharon Manne <aharon.manne@gmail.com>
2016-11-10 12:01:53 +0200

    Initial working version

---
+++ app/src/main/java/com/ksdagile/openapp/OpenAppWidgetProvider.java
@@ -0,0 +1,46 @@
+package com.ksdagile.openapp;
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Created by user on 08/11/2016.
+ */
+
+public class OpenAppWidgetProvider extends AppWidgetProvider {
+    private static final String ACTION_CLICK = "ACTION_CLICK";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                OpenAppWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_layout);
+
+            Toast.makeText(context,"Tapped OpenApp", Toast.LENGTH_LONG).show();
+            // Register an onClickListener
+            Intent intent = new Intent(context, OpenAppWidgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.toggle, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
